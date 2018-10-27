c596bc77ffe55367aeef333ecd4ece0512d766c5
Moataz7 <motazplus@gmail.com>
2018-10-27 16:13:53 +0200

    change to comparing the types

--- week1/script8.js
+++ week1/script8.js
@@ -15,32 +15,32 @@ console.log ("The type of w is :", typeof w);
 console.log ("The type of x is :", typeof x);
 console.log ("The type of y is :", typeof y);
 console.log ("The type of z is :", typeof z);
-if (w===x) {
+if (typeof w === typeof x) {
     console.log('W and X are the SAME TYPE');
 } else {
     console.log('W and X are NOT the SAME TYPE')
 }
-if (w===y) {
+if (typeof w === typeof y) {
     console.log('W and Y are the SAME TYPE');
 } else {
     console.log('W and Y are NOT the SAME TYPE')
 }
-if (w===z) {
+if (typeof w === typeof z) {
     console.log('W and Z are the SAME TYPE');
 } else {
     console.log('W and Z are NOT the SAME TYPE')
 }
-if (x===y) {
+if (typeof x === typeof y) {
     console.log('X and Y are the SAME TYPE');
 } else {
     console.log('X and Y are NOT the SAME TYPE')
 }
-if (x===z) {
+if (typeof x === typeof z) {
     console.log('X and Z are the SAME TYPE');
 } else {
     console.log('X and Z are NOT the SAME TYPE')
 }
-if (y===z) {
+if (typeof y === typeof z) {
     console.log('Y and Z are the SAME TYPE');
 } else {
     console.log('Y and Z are NOT the SAME TYPE')
