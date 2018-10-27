b42cba57829486c6b3d86548a8272f49021ef630
Moataz7 <motazplus@gmail.com>
2018-10-27 16:12:20 +0200

    correct the values

--- week1/script4.js
+++ week1/script4.js
@@ -1,7 +1,7 @@
 // 4. Declare a variable y and assign a string to it.
 var y = "Hello World" ;
-console.log ('the value of y will be: text');
+console.log ('the value of y will be: Hello World');
 console.log (y);
 y = "Hi Everyone"
-console.log ('the value of y will be: the new text');
+console.log ('the value of y will be: Hi Everyone');
 console.log(y);
